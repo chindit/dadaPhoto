8b35f841eab230c6e112fee13d4edcb9d8f36974
chindit <littletiger58@gmail.com>
2014-08-24 12:49:41 +0200

    Translator

--- main.cpp
+++ main.cpp
@@ -3,15 +3,16 @@
 #include <QTranslator>
 #include <QLocale>
 #include <QLibraryInfo>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    QString locale = QLocale::system().name().section('_', 0, 0);
-    QTranslator translator;
-    translator.load(QString("qt_") + locale, QLibraryInfo::location(QLibraryInfo::TranslationsPath));
-    app.installTranslator(&translator);
+    QTranslator frenchTranslator;
+    if (frenchTranslator.load("qt_" + QLocale::system().name())){
+        app.installTranslator(&frenchTranslator);
+    }
 
     dadaPhoto instance;
     instance.show();
